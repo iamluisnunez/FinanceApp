a8aa932c727efabfc0e3771418935aad1acc67a8
iamluisnunez <luis.nunez@mykenzie.snhu.edu>
2023-08-26 20:36:46 -0500

    Working on SQL

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -10,6 +10,30 @@ const getAdmin = (req, res) => {
   });
 };
 
+const getSingleAdmin = (req, res) => {
+  //   const str = `Select admin_name from admin where admin_id = ${req.params.id}`
+  //   res.send(req.params.id);
+  pool.query(
+    "Select * from users where user_id = $1",
+    [req.params.id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json(results.rows);
+      }
+    }
+  );
+};
+
+const newUser = (req, res) => {
+  const { username, email } = req.body;
+
+  res.send(username);
+};
+
 module.exports = {
   getAdmin,
+  getSingleAdmin,
+  newAdmin,
 };
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/admin", adminController.getAdmin);
+router.get("/admin/:id", adminController.getSingleAdmin);
+router.post("/admin", adminController.newUser);
 
 module.exports = router;
